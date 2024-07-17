9111662feebf7b053d192dd840642fa62cf3b554
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-07-17 22:58:31 +0530

    Basics

---
+++ operations.java
@@ -0,0 +1,10 @@
+public class operations {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        int sum = a + b;
+        int diff = a - b;
+        System.out.println(sum);
+        System.out.println(diff);
+    }
+}
