db12a796fccd34a12fd8e7b56531a64219fbab9a
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-07-17 19:31:25 +0530

    Basics

--- FirstClass.java
+++ FirstClass.java
@@ -1,5 +1,6 @@
 class FirstClass{
     public static void main(String args[]){
-        System.out.println("Hello");
+        System.out.println("Hello ayush");
+        System.out.println("Hello ayush");
     }
 }
