ff19e139cb6c8d036cf7dba21ec1b7148eb19797
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-07-22 09:30:11 +0530

    Basics

--- table.java
+++ table.java
@@ -6,7 +6,7 @@ public class table {
         System.out.println("Enter a number you want to print table of:");
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println(+num+"*"+i+"="+(num*i));
         }
         }
