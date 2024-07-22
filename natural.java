fe100d794142aa2603c9ba7061c06fa47887cf53
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-07-22 23:12:16 +0530

    Basics

--- natural.java
+++ natural.java
@@ -1,7 +1,12 @@
+
+import java.util.Scanner;
+
 public class natural {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         int sum = 0;
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i <= n; i++) {
             sum += i ;
  }
  System.out.println("Sum:"+sum);
