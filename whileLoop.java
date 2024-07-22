396d1e75c67ba09b25adba8992cebcbd75bc5506
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-07-22 23:18:30 +0530

    Basics

--- whileLoop.java
+++ whileLoop.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 class whileLoop{
     public static void main(String[] args) {
-        int i = 0;
-        while (i<10){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int i = 1;
+        while(i<=10){
+            System.out.println(num+"*"+i+"="+(num*i));
             i = i + 1;
-            System.out.println(i);
         }
+        System.out.println("");
     }
 }
