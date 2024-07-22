8443dc80034654e8fcf48b845afe488ccc6d6f30
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-07-22 23:40:09 +0530

    Basics

--- pattern1.java
+++ pattern1.java
@@ -1,7 +1,12 @@
 public class pattern1 {
     public static void main(String[] args) {
-        for (int i = 1;i<=4;i++){
-            System.out.println("*\t*\t*\t*\t*");
+        int c = 4;
+        int r = 5;
+        for (int i = 1;i<=c;i++){
+            for(int j = 1;j<=r;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
         }
     }
 }
