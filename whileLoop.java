cad1851c2828fac88699168f3a69ed0ab600fd58
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-07-22 22:52:12 +0530

    Basics

--- whileLoop.java
+++ whileLoop.java
@@ -1,6 +1,8 @@
-public class loops {
+class whileLoop{
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
+        int i = 0;
+        while (i<10){
+            i = i + 1;
             System.out.println(i);
         }
     }
