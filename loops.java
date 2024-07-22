bdf0b25f37aad454eaa5d5486b8af9de57bcf67c
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-07-22 09:23:13 +0530

    Basics

--- loops.java
+++ loops.java
@@ -1,7 +1,7 @@
 public class loops {
     public static void main(String[] args) {
         for (int i = 1; i < 10; i++) {
-            System.out.println(2*i);
+            System.out.println(i);
         }
     }
 }
