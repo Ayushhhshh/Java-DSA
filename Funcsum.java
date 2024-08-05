8d3b151af79d04f3ba7728528925d2220d6ff2c1
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-08-05 22:20:30 +0530

    Basics

---
+++ Funcsum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Funcsum {
+    public static int sum(int a , int b){
+        int sum = a + b;
+        return sum;         
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter two number:");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+         int plus = sum(a, b);
+         System.out.println(plus);
+    }
+}
