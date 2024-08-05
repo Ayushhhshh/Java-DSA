d30fc990df41bc51b5ee4407aac07ba0269edef5
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-08-05 22:01:46 +0530

    Basics

--- functions.java
+++ functions.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class functions {
- public static void main(String[] args) {
-    System.out.println();
+ public static void printName(String name) {
+    System.out.println(name);
+    return;
  }   
+
+ public static void main(String[]args){
+   System.out.println("Enter your name:");
+   Scanner sc = new Scanner(System.in);
+   String name = sc.next();
+
+   printName(name);
+}
 }
