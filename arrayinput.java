cbbd900c41ec3bc211dd67e35ce4eb8d45d1db70
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-08-12 14:13:08 +0530

    Arrays

--- arrayinput.java
+++ arrayinput.java
@@ -1,11 +1,12 @@
 
 import java.util.Scanner;
 
-public class arrayinput {
-    public static void main(String[]args){
+public class arrayinput{
+    public static void main(String[] args) {
+        System.out.print("Enter the size of array: ");
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        int arr[] = new int [size];
+        int arr[] = new int[size];
 
         for (int i = 0; i < size; i++) {
             System.out.println(arr[i]);
