a5a378401903a4f4d15de55eef6c644dc0ca1f02
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-08-12 15:59:53 +0530

    Arrays

--- secondLargest.java
+++ secondLargest.java
@@ -1,14 +1,17 @@
 public class secondLargest {
     public static void main(String[] args) {
-       int arr[]={10,98,99,97,1,2,11,25};
+       int arr[]={10,9,99,97,1,2,11,25,98};
        int largest = arr[0];
-       int sec = 0;
-       for(int i = 0; i<=arr.length-1;i++){
-          if(arr[i]>largest){
-              sec = largest;
+       int sec = -1;
+       for(int i = 0; i< arr.length;i++){
+          if(arr[i]>largest ){
               largest = arr[i];
           }
-       }
+        }
+        for (int i = 0; i < arr.length; i++) {
+         if(arr[i]!=largest && sec<arr[i])
+         sec = arr[i];
+     }
        System.out.println(sec);
     }
 }
