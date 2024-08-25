5864e3bc300e6eb8b96871c79c3b731c8b7a7967
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-08-26 02:32:09 +0530

    Searching

---
+++ linearSearch.java
@@ -0,0 +1,13 @@
+public class linearSearch {
+    public static void main(String[] args) {
+        int arr[]={1,5,3,7,6,9};
+        int num = 9;
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==num){
+                System.out.println("The index of num is "+i);
+                break;
+            }
+        }
+    }
+}
