a838cf93e74506f993f6ab3f34e06845e4c1d6fc
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-08-26 02:24:54 +0530

    Arrays

--- moveZeroToEndWithoutNewArray.java
+++ moveZeroToEndWithoutNewArray.java
@@ -1,3 +1,4 @@
+
 public class moveZeroToEndWithoutNewArray {
     public static void main(String[] args) {
         int arr[] = {1,2,0,3,0,4,5,0,6};
@@ -12,7 +13,7 @@ public class moveZeroToEndWithoutNewArray {
         }
         for (int i = j+1; i < arr.length; i++) {
             if(arr[i]!=0){
-                swap(arr[i],arr[j]);
+                swapElementsIndex(arr, i, j);
                 j++;
 
             }
@@ -20,5 +21,10 @@ public class moveZeroToEndWithoutNewArray {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
+    }
+    public static void swapElementsIndex(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
