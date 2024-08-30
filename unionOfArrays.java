9ad22bf6c8506bf10d9fd334d6d027cf52c7b2d8
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-08-31 00:17:59 +0530

    Arrays

---
+++ unionOfArrays.java
@@ -0,0 +1,11 @@
+public class unionOfArrays{
+    public static void main(String[] args) {
+        int arr [] = {1,1,2,3,4,5};
+        int arr2[] = {2,3,4,5};
+        int temp[] = new int [arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            
+        }
+    }
+}
