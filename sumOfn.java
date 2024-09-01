99a3d16e7ffb1dcfcd44c113e90416d3bbd9b790
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-09-01 19:57:12 +0530

    Basics

--- sumOfn.java
+++ sumOfn.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 
 public class sumOfn {
     public static void main(String[] args) {
-        System.out.println("Enter the number: ");
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        System.out.println("Enter the Starting integer: ");
+        int start = sc.nextInt();
+        System.out.println("Enter the Ending integer: ");
+        int stop = sc.nextInt();
         int sum = 0;
 
-        for (int i = 1; i <=n; i++) {
+        for (int i = start; i <= stop; i++) {
             sum = sum+i;
         }
         System.out.println(sum);
