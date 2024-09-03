f1e4fecb679919348c602c14dd8b07c2a971cebe
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-09-04 00:26:08 +0530

    Arrays

---
+++ rotateNegative.java
@@ -0,0 +1,12 @@
+public class  rotateNegative{
+    public static void main(String[] args) {
+        int arr [] = {1,2,3,-5,-4};
+        int j = 0;
+
+        for(int i = 0;i<arr.length)
+        
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+   }
