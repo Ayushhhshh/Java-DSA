7cb585c2d0ea3394fe78a1fd664c0a82f2f5d6ee
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-09-04 01:09:38 +0530

    Arrays

--- rotateNegative.java
+++ rotateNegative.java
@@ -2,10 +2,16 @@ public class  rotateNegative{
     public static void main(String[] args) {
         int arr [] = {1,2,3,-5,-4};
         int j = 0;
+        int a = 0;
 
-        for(int i = 0;i<arr.length)
-        
-        for(int i = 0; i < arr.length; i++){
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i]<0){
+                a = arr[0];
+                arr[0]=arr[i];
+            }
+        }
+
+        for(int i = 0; i<arr.length;i++){
             System.out.println(arr[i]);
         }
     }
