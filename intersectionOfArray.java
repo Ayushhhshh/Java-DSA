7638d8a58240e9ecf04f8abdbcd1407e73b4c0b4
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-09-05 00:42:33 +0530

    Arrays

--- intersectionOfArray.java
+++ intersectionOfArray.java
@@ -1,3 +1,21 @@
 public class intersectionOfArray {
-    
+    public static void main(String[] args) {
+        
+    int arr [] = {1,2,2,3,3,4,5,6};
+    int arr2[] = {2,3,3,5,6,7};
+    int ins[] = new int[arr.length];
+
+    for(int i=0; i<arr.length;i++){
+        for (int j = 0; j < arr2.length; j++) {
+            if(arr[i]==arr2[j]){
+                ins[i]=arr[i];
+            }
+            
+        }
+    }
+
+        for (int i = 0; i < ins.length; i++) {
+            System.out.println(ins[i]);
+        }
+    }
 }
