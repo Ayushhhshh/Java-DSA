9c955133a70005f1dc173b453e288985918faa18
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-09-09 01:03:20 +0530

    Basics

--- isPrime.java
+++ isPrime.java
@@ -1,11 +1,12 @@
 class isPrime {
     public static void main(String[] args) {
-        int n = 12;
+        int n = 31;
         boolean code = true;
         
         for(int i = 2 ; i < n; i++){
             if(n % i == 0){
                 code = false;
+                break;
             }
         }
         System.out.println("Is "+n+" a prime number : "+code);
