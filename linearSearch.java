92062da0c085cde98a2f09a354532e9a6a423399
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-09-10 22:41:57 +0530

    Linear search

--- linearSearch.java
+++ linearSearch.java
@@ -1,6 +1,6 @@
 public class linearSearch {
     public static void main(String[] args) {
-        int arr[]={1,5,3,7,6,9};
+        int arr[]={1,5,3,7,6,7,5,9};
         int num = 9;
 
         for (int i = 0; i < arr.length; i++) {
